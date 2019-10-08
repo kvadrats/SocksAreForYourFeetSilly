3c855aac4d97e6d5d6a2997cb3439ae41b8121c5
Oscillation71 <ilva.danenberga@sonarworks.com>
2019-10-08 17:33:35 +0300

    last test

--- tests/api/users/test_user_registration.py
+++ tests/api/users/test_user_registration.py
@@ -4,8 +4,12 @@ from tests.api.users.common import UserTestCase
 class TestUserRegistration(UserTestCase):
     def test_user_registration(self):
         payload = self.requests.REGISTRATION_FORM
-        payload["username"] = "timmy"
-        payload["password"] = "yourmom"
-        payload["email"] = "emial@emal.lv"
+        payload["username"] = "test"
+        payload["password"] = "test2"
+        payload["email"] = "em@email.lv"
         result = self.user.send_post_request(payload)
+        reg_id = result['id']
+
+    def test_registration_success(self):
+        result = self.USER_GET_CUSTOMERID.send_get_request()
         print(result)
